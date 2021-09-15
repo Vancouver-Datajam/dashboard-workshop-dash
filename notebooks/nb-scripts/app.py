d28da93851cd2ebea3d3853e3712918503e2819f
hanhtong <47369849+hanhtong@users.noreply.github.com>
2021-09-15 15:35:17 -0700

    Modify app.py

--- notebooks/nb-scripts/app.py
+++ notebooks/nb-scripts/app.py
@@ -5,6 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+
 def graph_region(region_df, graph_type: str, dimension1: str, dimension2: str) -> None:
     """
     Parameters
@@ -24,10 +25,10 @@ def graph_region(region_df, graph_type: str, dimension1: str, dimension2: str) -
     try:
         # Initialize function
         fig = plot_dict[graph_type](region_df, 
-                                    x=dimension1, 
-                                    y=dimension2, 
+                                    x = dimension1, 
+                                    y = dimension2, 
                                     color = "Geography",
-                                   hover_name = "Time")
+                                    hover_name = "Time")
         # Format figure 
         title_string = f'Chart: {graph_type} plot of {dimension1} and {dimension2} by Geography'
         fig.update_layout(title = title_string)
@@ -41,6 +42,7 @@ def graph_region(region_df, graph_type: str, dimension1: str, dimension2: str) -
         print("dimension2 is one of 'AverageMortgageAmount', 'DelinquencyRate', 'PopulationSize'")
         
         
+        
 # ----------------------------------------------------------------------------------#
 # Read data
 
@@ -49,7 +51,6 @@ data_pop_del_mort_df = pd.read_csv(url, index_col=0)
 
 # ----------------------------------------------------------------------------------#
 # App section        
-        
 
 # Stylesheets
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -58,17 +59,48 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+
 app.layout = html.Div([
+    html.Div([
         html.H1("Housing graphs"),
         dcc.Dropdown(
             id='province',
             options=[{'label': i, 'value': i} for i in data_pop_del_mort_df['Geography'].unique()],
             value= 'Newfoundland'
         )
-    ])
-# ----------------------------------------------------------------------------------#
-# Read data
-        
-if __name__ == '__main__':  
+    ]),
     
+    html.Div([
+        html.Div([
+            dcc.Graph(id='graph-time-mortgage', figure={'data': [{'y': [1, 2, 3]}]})
+        ], className="six columns"),
+
+        html.Div([
+            dcc.Graph(id='graph-time-del', figure={'data': [{'y': [1, 2, 3]}]})
+        ], className="six columns"),
+    ], className="row")
+])
+
+      
+    
+@app.callback(
+    Output('graph-time-mortgage', 'figure'),
+    Input('province', 'value'))
+def update_figure1(selected_province):
+    df = data_pop_del_mort_df
+    filtered_df = df[df['Geography'] == selected_province]
+    fig1 = graph_region(filtered_df, 'line', "Time", "AverageMortgageAmount")
+    return fig1
+
+@app.callback(
+    Output('graph-time-del', 'figure'),
+    Input('province', 'value'))
+def update_figure2(selected_province):
+    df = data_pop_del_mort_df
+    filtered_df = df[df['Geography'] == selected_province]  
+    fig2 = graph_region(filtered_df, 'line', "Time", "DelinquencyRate")
+    return fig2
+
+# ----------------------------------------------------------------------------------#
+if __name__ == '__main__':    
     app.run_server(debug=True) 
