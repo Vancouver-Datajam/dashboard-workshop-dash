d4333024fc8a78100a6ccecfdb56dfba4bbc894e
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2021-09-16 17:30:08 -0700

    add three examples of layouts

--- notebooks/nb-scripts/app.py
+++ notebooks/nb-scripts/app.py
@@ -59,6 +59,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+
+# ----------------------------------------------------------------------------------#
+# A dropdown menu and a chart
+
 app.layout = html.Div([
              # This div contains a header H1, a dropdown to select the kind of plot and the plot
             html.H1("Different kinds of plots"),
@@ -72,89 +76,142 @@ app.layout = html.Div([
         
 ])
 
-app.layout = html.Div([
-    html.Div(
-        className="six columns",
-        children = [
-            html.H1("Housing graphs"),
-                dcc.Dropdown(
-                    id='province',
-                    options=[{'label': i, 'value': i} for i in data_pop_del_mort_df['Geography'].unique()],
-                    value= 'Newfoundland'
-                ),
-            
-            html.Div([
-                   dcc.Graph(id='graph-time-mortgage')
-                    ], className="six columns"),
-            
-            html.Div([
-                dcc.Graph(id='graph-time-del')
-                ], className="six columns"),
-            
-            ]),
-    
-    html.Div(
-        className="six columns",
-        children = [
-            html.Div([
-            dcc.Graph(id='scatter-mortgage-del')
-            ]),
-        ])
-])
-
-
-    
 @app.callback(
-    Output('graph-time-mortgage', 'figure'),
-    Input('province', 'value'))
-def update_figure1(selected_province):
-    df = data_pop_del_mort_df
-    filtered_df = df[df['Geography'] == selected_province]
-    fig1 = graph_region(filtered_df, 'line', "Time", "AverageMortgageAmount")
-    return fig1
-
-@app.callback(
-    Output('graph-time-del', 'figure'),
-    Input('province', 'value'))
-def update_figure2(selected_province):
-    df = data_pop_del_mort_df
-    filtered_df = df[df['Geography'] == selected_province]  
-    fig2 = graph_region(filtered_df, 'line', "Time", "DelinquencyRate")
-    return fig2
-
-@app.callback(
-    Output('scatter-mortgage-del', 'figure'),
-    Input('province', 'value'))
-def update_figure3(selected_province):
-    df = data_pop_del_mort_df
-    filtered_df = df[df['Geography'] == selected_province]  
-    fig3 = graph_region(filtered_df, 'scatter', "AverageMortgageAmount", "DelinquencyRate")
-    return fig3
-        
-if __name__ == '__main__':  
-    
-    app.run_server(debug=True) 
+    Output('graph-render', 'figure'),
+    Input('graph-type', 'value'))
+def update_figure0(selected_graph):
+    filtered_df = data_pop_del_mort_df
+    fig0 = graph_region(filtered_df, selected_graph, "Geography", "AverageMortgageAmount")
+    return fig0
+# ----------------------------------------------------------------------------------#
 
     
+# ----------------------------------------------------------------------------------#
+# Improving aesthetics, dropdown that changes three graphs
+
 # app.layout = html.Div([
 #     html.Div(
-#         className="row",
+#         className="six columns",
 #         children = [
-#                 html.H1("Housing graphs"),
+#             html.H1("Housing graphs"),
 #                 dcc.Dropdown(
 #                     id='province',
 #                     options=[{'label': i, 'value': i} for i in data_pop_del_mort_df['Geography'].unique()],
 #                     value= 'Newfoundland'
-#                 )
-#             ]),
-
-#         html.Div([
+#                 ),
+            
 #             html.Div([
-#                 dcc.Graph(id='graph-time-mortgage')
-#             ], className="six columns"),
-
+#                    dcc.Graph(id='graph-time-mortgage')
+#                     ], className="six columns"),
+            
 #             html.Div([
 #                 dcc.Graph(id='graph-time-del')
-#             ], className="six columns"),
+#                 ], className="six columns"),
+            
+#             ]),
+    
+#     html.Div(
+#         className="six columns",
+#         children = [
+#             html.Div([
+#             dcc.Graph(id='scatter-mortgage-del')
+#             ]),
 #         ])
 # ])
+
+
+    
+# @app.callback(
+#     Output('graph-time-mortgage', 'figure'),
+#     Input('province', 'value'))
+# def update_figure1(selected_province):
+#     df = data_pop_del_mort_df
+#     filtered_df = df[df['Geography'] == selected_province]
+#     fig1 = graph_region(filtered_df, 'line', "Time", "AverageMortgageAmount")
+#     return fig1
+
+# @app.callback(
+#     Output('graph-time-del', 'figure'),
+#     Input('province', 'value'))
+# def update_figure2(selected_province):
+#     df = data_pop_del_mort_df
+#     filtered_df = df[df['Geography'] == selected_province]  
+#     fig2 = graph_region(filtered_df, 'line', "Time", "DelinquencyRate")
+#     return fig2
+
+# @app.callback(
+#     Output('scatter-mortgage-del', 'figure'),
+#     Input('province', 'value'))
+# def update_figure3(selected_province):
+#     df = data_pop_del_mort_df
+#     filtered_df = df[df['Geography'] == selected_province]  
+#     fig3 = graph_region(filtered_df, 'scatter', "AverageMortgageAmount", "DelinquencyRate")
+#     return fig3
+
+# ----------------------------------------------------------------------------------#
+# text_style = {
+#     'textAlign' : 'center',
+#     'color' : "black"
+# }
+
+# card_text_style = {
+#     'textAlign' : 'center',
+#     'color' : 'black'
+# }
+    
+# app.layout = html.Div([
+#     html.Div([
+#         html.H2("Housing Market Trends in Vancouver (quarterly, 2012 - 2020)", style=card_text_style),
+#         html.Div([
+            
+#             dcc.Dropdown(
+#                 id='xaxis-column',
+#                 options=[{'label': 'Geography', 'value': 'Geography'},
+#                          {'label': 'Time', 'value': 'Time'},
+#                         {'label': 'Population Size', 'value': 'PopulationSize'},
+#                          {'label': 'Delinquency Rate', 'value': 'DelinquencyRate'},
+#                         {'label': 'Average Mortgage Amount', 'value': 'AverageMortgageAmount'}],
+#                 value='Geography'
+#             ),
+#             dcc.Dropdown(
+#                 id='yaxis-column',
+#                 options=[{'label': 'Population Size', 'value': 'PopulationSize'},
+#                          {'label': 'Delinquency Rate', 'value': 'DelinquencyRate'},
+#                         {'label': 'Average Mortgage Amount', 'value': 'AverageMortgageAmount'}],
+#                 value='PopulationSize'
+#             ),
+            
+#         ]),
+        
+#         html.Div([
+#             dcc.Checklist(
+#                 id='graph-type',
+#                 options=[{'label': 'Violin plot', 'value': 'violin'},
+#                          {'label': 'Box plot', 'value': 'box'},
+#                         {'label': 'Scatter plot', 'value': 'scatter'},
+#                         {'label': 'Line plot', 'value': 'line'}],
+#                 value=['violin']
+#             )
+#         ])
+#     ]),
+
+#     dcc.Graph(id='indicator-graphic'),
+
+    
+# ])
+
+# @app.callback(
+#     Output('indicator-graphic', 'figure'),
+#     Input('graph-type', 'value'),
+#     Input('xaxis-column', 'value'),
+#     Input('yaxis-column', 'value'))
+# def update_figure3(selected_graph, xaxis, yaxis):
+#     filtered_df = data_pop_del_mort_df
+#     fig3 = graph_region(filtered_df, selected_graph[0], xaxis, yaxis)
+#     return fig3
+
+# ----------------------------------------------------------------------------------#
+
+if __name__ == '__main__':  
+    
+    app.run_server(debug=True) 
